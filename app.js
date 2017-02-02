b0d45154612d0f1c0e690ff274c077acd3959e30
恵玉 李 <support@deviewstory.com>
2017-02-01 23:04:07 -0600

    てっとり早い

--- app.js
+++ app.js
@@ -15,6 +15,41 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+// ----------ここから----------
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+// --------ここまで追加--------
+
+// ----------ここから----------
+var request = require('request');
+
+app.post('/api', function(req, res) {
+  var options = {
+    method: 'POST',
+    uri: 'https://api.line.me/v2/bot/message/reply',
+    body: {
+      replyToken: req.body.events[0].replyToken,
+      messages: [{
+        type: "text",
+        text: req.body.events[0].message.text
+      }]
+    },
+    auth: {
+      bearer: 'rOUF+RwMgI16TO9EAOxm4qa2aPhol/REt0LkV9IvUSlWewd+pSFQ3c9Q/1BLk3fXoJsIhxvHiSn9sDiIeKtesjGiPi02vMx8VhIHtIy0PkoLM5kDdampTyc+WyaHi+hTHk6B4TH/LJL3PtcNV9srmAdB04t89/1O/w1cDnyilFU=' // ここは自分のtokenに書き換える
+    },
+    json: true
+  };
+  request(options, function(err, res, body) {
+    console.log(JSON.stringify(res));
+  });
+  res.send('OK');
+});
+// --------ここまで追加--------
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
