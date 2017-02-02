d5b0138bcfb73d6462bf9233e591da5a52fa2d29
恵玉 李 <support@deviewstory.com>
2017-02-02 07:54:47 -0600

    トライ

--- app.js
+++ app.js
@@ -15,6 +15,30 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+// ----------ここから自前コード----------
+var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+
+var gTbl = {};
+
+function putVal(pKey, key, val){
+  var mTbl = gTbl[pKey];
+  if(mTbl == 'null'){
+    mTbl = {};
+    mTbl[key] = val;
+    gTbl[pKey] = mTbl;
+  }else{
+    mTbl[key] = val;
+  }
+}
+
+function getVal(pKey, key){
+  if(gTbl[pKey] == 'null' || gTbl[pKey][key] == 'null'){
+    return null;
+  }
+  return gTbl[pKey][key];
+}
+// ----------ここまで自前コード----------
+
 // ----------ここから----------
 var bodyParser = require('body-parser');
 
@@ -24,31 +48,83 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 // --------ここまで追加--------
 
+// ----------ここから自前コード----------
+// Create the service wrapper
+var conversation = new Conversation({
+  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+  // username: '<username>',
+  // password: '<password>',
+  url: 'https://gateway.watsonplatform.net/conversation/api',
+  version_date: '2016-10-21',
+  version: 'v1'
+});
+// ----------ここまで自前コード----------
+
 // ----------ここから----------
 var request = require('request');
 
 app.post('/api', function(req, res) {
-  var options = {
-    method: 'POST',
-    uri: 'https://api.line.me/v2/bot/message/reply',
-    body: {
-      replyToken: req.body.events[0].replyToken,
-      messages: [{
-        type: "text",
-        text: "fuck off"
-      }]
+
+  // Extract data from LINE API request
+  var body = JSON.parse(req.body); 
+  var data = body.events[0];
+  if(data.type !== 'message'){
+    console.log(data.type);
+    return;
+  }
+  var replyToken = data.replyToken;
+  var userId = data.source.userId;
+  var message = data.message.text;
+  message = message.replace(/\n/g,' ');
+
+  // Set reqest for watson
+  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+  if (!workspace || workspace === '<workspace-id>') {
+    return res.json({
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    });
+  }
+  var reqStr = JSON.stringify({
+    workspace_id: workspace,
+    context: getVal(userId, "watson_context"),
+    input: {text: message }
+  });
+
+  // Send the input to the conversation service
+  var resStr = {};
+  conversation.message(reqStr, function(err, data) {
+    if (err) {
+      resStr.status(err.code || 500).json(err);
+    }
+      resStr.json(updateMessage(reqStr, data));
+  });
+
+  /// Watson reply message from conversation
+  var res = JSON.stringify({
+    replyToken: replyToken,
+    messages: [
+      {type: "text", text: resStr.output}
+  ]});
+  
+  // Set LINE api access
+  var lineOpts = {
+    hostname: 'api.line.me',
+    path: '/v2/bot/message/reply',
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "Authorization": "Bearer " + process.env.LINE_TOKEN
     },
-    auth: {
-      bearer: 'rOUF+RwMgI16TO9EAOxm4qa2aPhol/REt0LkV9IvUSlWewd+pSFQ3c9Q/1BLk3fXoJsIhxvHiSn9sDiIeKtesjGiPi02vMx8VhIHtIy0PkoLM5kDdampTyc+WyaHi+hTHk6B4TH/LJL3PtcNV9srmAdB04t89/1O/w1cDnyilFU=' // ここは自分のtokenに書き換える
-    },
-    json: true
+    method: 'POST'
   };
-  request(options, function(err, res, body) {
+
+  request(lineOpts, function(err, res, body) {
     console.log(JSON.stringify(res));
   });
   res.send('OK');
-});
-// --------ここまで追加--------
+
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
